c6e68800779ac23ed29dddfb4fe8d8c9d9939392
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-17 01:00:37 +0300

    FileService

---
+++ src/main/java/com/company/service/FileService.java
@@ -0,0 +1,38 @@
+package com.company.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Service
+public class FileService {
+
+    public String getFileContent(MultipartFile file) {
+        String content = "";
+        try {
+            content = new String(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+    public String getFileContent(String filePath) {
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+    public void setFileWithContent(String filePath, String content) throws IOException {
+        FileWriter writer = new FileWriter(filePath, false);
+        writer.write(content);
+        writer.flush();
+    }
+
+}
