69ed5cce453f51957f07b74f7dced65edaa6f733
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-17 01:01:38 +0300

    Addition postMapping to test, and autowired needs services

--- src/main/java/com/company/controller/JSONController.java
+++ src/main/java/com/company/controller/JSONController.java
@@ -1,33 +1,33 @@
 package com.company.controller;
 
-
-
 import com.company.model.JSONStructure;
+import com.company.service.FileService;
+import com.company.service.JSONService;
 import com.company.service.SchemaService;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.NamedType;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 @Controller
 public class JSONController {
     @Autowired
     private SchemaService schemaService;
-
+    @Autowired
+    private FileService fileService;
+    @Autowired
+    private JSONService jsonService;
     @GetMapping("/")
-    public String getStartPage() throws IOException {
-        schemaService.schemaGenerate(JSONStructure.class, "1.json");
-        schemaService.getSchemaErrors("2.json", "22.json").forEach(System.out::println);
-
-        ObjectMapper mapper = new ObjectMapper();
-        //JSONStructure obj = mapper.readValue(j, JSONStructure.class);
-        //System.out.println(obj.getArtifacts().get(0).toPrettyString());
+    public String getStartPage() {
+        schemaService.getSchemaErrors(schemaService.getJsonSchema(JSONStructure.class), jsonService.getJsonNode(fileService.getFileContent("22.json"))).forEach(System.out::println);
         return "start";
+    }
+    @PostMapping("/")
+    public String getResponsePage(@RequestParam(name = "file1") MultipartFile multipartFile) {
+        System.out.println(fileService.getFileContent(multipartFile));
+        return "responsePage";
     }
 }
