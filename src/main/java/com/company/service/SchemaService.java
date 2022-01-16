22be03a7ea2fb67421cebc125e418f831d48d13a
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-17 01:03:32 +0300

    refactored methods

--- src/main/java/com/company/service/SchemaService.java
+++ src/main/java/com/company/service/SchemaService.java
@@ -1,7 +1,6 @@
 package com.company.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.imifou.jsonschema.module.addon.AddonModule;
 import com.github.victools.jsonschema.generator.*;
 import com.github.victools.jsonschema.module.jackson.JacksonModule;
@@ -10,13 +9,12 @@ import com.github.victools.jsonschema.module.javax.validation.JavaxValidationMod
 import com.networknt.schema.*;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
 import java.util.Set;
 
 @Service
 public class SchemaService {
 
-    public void schemaGenerate(Class<?> clazz, String pathFile) throws IOException {
+    public JsonNode getJsonSchema(Class<?> clazz) {
         JacksonModule jacksonModule = new JacksonModule(JacksonOption.RESPECT_JSONPROPERTY_REQUIRED);
         JavaxValidationModule javaxValidationModule = new JavaxValidationModule();
         AddonModule addonModule= new AddonModule();
@@ -24,17 +22,11 @@ public class SchemaService {
                 .with(jacksonModule).with(javaxValidationModule).with(addonModule);
         SchemaGeneratorConfig config = configBuilder.build();
         SchemaGenerator generator = new SchemaGenerator(config);
-        JsonNode jsonSchema = generator.generateSchema(clazz);
-        FileWriter writer = new FileWriter(pathFile, false);
-        writer.write(jsonSchema.toString());
-        writer.flush();
+        return generator.generateSchema(clazz);
     }
 
-    public Set<ValidationMessage> getSchemaErrors(String schemaFilePath, String nodeFilePath) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
+    public Set<ValidationMessage> getSchemaErrors(JsonNode schema, JsonNode node) {
         JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
-        JsonSchema schema = factory.getSchema(new FileInputStream(schemaFilePath));
-        JsonNode node = mapper.readTree(new File(nodeFilePath));
-        return schema.validate(node);
+        return factory.getSchema(schema).validate(node);
     }
 }
