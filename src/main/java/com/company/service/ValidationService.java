e161e7bc9b205dd61febf14fdfee5b1ad42ebb59
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-20 14:33:33 +0300

    generic service for json validation

---
+++ src/main/java/com/company/service/ValidationService.java
@@ -0,0 +1,22 @@
+package com.company.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ValidationService {
+    @Autowired
+    private SchemaService schemaService;
+    @Autowired
+    private FileService fileService;
+    @Autowired
+    private JSONService jsonService;
+
+    public String getErrors(MultipartFile inputFile) {
+        //check ArtifactObject1-2 and Parameter.Services.ServiceName maps
+        return "";
+    }
+
+
+}
