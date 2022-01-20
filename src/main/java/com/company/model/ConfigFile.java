333548786a8178898a27816c80c24fb2e8397079
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-20 14:32:39 +0300

    Main model

---
+++ src/main/java/com/company/model/ConfigFile.java
@@ -0,0 +1,76 @@
+package com.company.model;
+
+import com.company.pojo.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ConfigFile {
+    private Metadata metadata;
+    private ArrayList<Service> services;
+    private ArrayList<ArtifactObject> artifacts;
+    private ArrayList<Script> script;
+    private ArrayList<Rpm> rpm;
+    private Parameter parameters;
+
+    public ConfigFile(Metadata metadata, ArrayList<Service> services, ArrayList<ArtifactObject> artifacts, ArrayList<Script> script, ArrayList<Rpm> rpm, Parameter parameters) {
+        this.metadata = metadata;
+        this.services = services;
+        this.artifacts = artifacts;
+        this.script = script;
+        this.rpm = rpm;
+        this.parameters = parameters;
+    }
+
+    public ConfigFile(JSONStructure jsonStructure) {
+
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public ArrayList<Service> getServices() {
+        return services;
+    }
+
+    public ArrayList<ArtifactObject> getArtifacts() {
+        return artifacts;
+    }
+
+    public ArrayList<Script> getScript() {
+        return script;
+    }
+
+    public ArrayList<Rpm> getRpm() {
+        return rpm;
+    }
+
+    public Parameter getParameters() {
+        return parameters;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //needs implementation
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        //rpm not required and can be empty...
+        return Objects.hash(metadata, services, artifacts, script, rpm, parameters);
+    }
+
+    @Override
+    public String toString() {
+        return "ConfigFile{" +
+                "metadata=" + metadata +
+                ", services=" + services +
+                ", artifacts=" + artifacts +
+                ", script=" + script +
+                ", rpm=" + rpm +
+                ", parameters=" + parameters +
+                '}';
+    }
+}
