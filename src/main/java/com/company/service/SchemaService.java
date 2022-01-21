7bcd62e36e03b9bedec01a91ba4e55e1cab044d8
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-21 15:49:05 +0300

    - AddonModule

--- src/main/java/com/company/service/SchemaService.java
+++ src/main/java/com/company/service/SchemaService.java
@@ -1,9 +1,6 @@
 package com.company.service;
 
-import com.company.pojo.*;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
-import com.github.imifou.jsonschema.module.addon.AddonModule;
 import com.github.victools.jsonschema.generator.*;
 import com.github.victools.jsonschema.module.jackson.JacksonModule;
 import com.github.victools.jsonschema.module.jackson.JacksonOption;
@@ -11,9 +8,6 @@ import com.github.victools.jsonschema.module.javax.validation.JavaxValidationMod
 import com.networknt.schema.*;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -22,15 +16,10 @@ public class SchemaService {
     public JsonNode getJsonSchema(Class<?> clazz) {
         JacksonModule jacksonModule = new JacksonModule(JacksonOption.RESPECT_JSONPROPERTY_REQUIRED);
         JavaxValidationModule javaxValidationModule = new JavaxValidationModule();
-        AddonModule addonModule= new AddonModule(); //2022-01-17 - dont use
         SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2019_09, OptionPreset.PLAIN_JSON)
                 .with(jacksonModule)
                 .with(javaxValidationModule)
-                .with(addonModule)
                 .with(Option.FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT);
-
-                //.with(Option.INLINE_ALL_SCHEMAS)
-
         SchemaGeneratorConfig config = configBuilder.build();
         SchemaGenerator generator = new SchemaGenerator(config);
         return generator.generateSchema(clazz);
