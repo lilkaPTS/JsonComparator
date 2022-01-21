7272a197b73055c41dfdd2d1c148d1a2387de276
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-21 04:16:12 +0300

    now controller printed errors in terminal this is function to development

--- src/main/java/com/company/controller/JsonController.java
+++ src/main/java/com/company/controller/JsonController.java
@@ -1,10 +1,14 @@
 package com.company.controller;
 
+import com.company.model.ArtifactObject1;
+import com.company.model.ArtifactObject2;
+import com.company.pojo.Artifacts;
 import com.company.pojo.JsonStructure;
 import com.company.service.FileService;
 import com.company.service.JsonService;
 import com.company.service.SchemaService;
 import com.company.service.ValidationService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,15 +31,12 @@ public class JsonController {
 
     @GetMapping("/")
     public String getStartPage() {
-        schemaService.getSchemaErrors(schemaService.getJsonSchema(JsonStructure.class), jsonService.getJsonNode(fileService.getFileContent("./src/main/resources/v2_json_sample_2.json"))).forEach(System.out::println);
-        fileService.setFileWithContent("JsonSchema.json", schemaService.getJsonSchema(JsonStructure.class).toString());
-        ObjectMapper mapper = new ObjectMapper();
-        //JsonStructure jsonStructure = mapper.readValue(fileService.getFileContent("./src/main/resources/v2_json_sample.json"), JsonStructure.class);
         return "start";
     }
+
     @PostMapping("/")
-    public String getResponsePage(@RequestParam(name = "file1") MultipartFile multipartFile) {
-        validationService.getErrors(multipartFile);
+    public String getResponsePage(@RequestParam(name = "file1") MultipartFile multipartFile) throws JsonProcessingException {
+        System.out.println(validationService.getErrors(multipartFile));
         return "responsePage";
     }
 }
