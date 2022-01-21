08494e1669f87029bda197f963932803caca52e3
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-21 04:12:14 +0300

    now getErrors function can catch errors from main Object and artifactObjects

--- src/main/java/com/company/service/ValidationService.java
+++ src/main/java/com/company/service/ValidationService.java
@@ -1,8 +1,20 @@
 package com.company.service;
 
+import com.company.model.ArtifactObject1;
+import com.company.model.ArtifactObject2;
+import com.company.pojo.Artifacts;
+import com.company.pojo.JsonStructure;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.ValidationMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Service
 public class ValidationService {
@@ -13,10 +25,52 @@ public class ValidationService {
     @Autowired
     private JsonService jsonService;
 
-    public String getErrors(MultipartFile inputFile) {
-        //check ArtifactObject1-2 and Parameter.Services.ServiceName maps
-        return "";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+
+    /**
+     * At first, let's take errors by JsonSchema.json, then checked
+     * founded errors to contains "null found" or "artifact is missing",
+     * if this errors not found, we will set ArrayList of JsonNode based
+     * Artifacts.class which is helper POJO to parse ArtifactObject1-2,
+     * then we will check each JsonNode to errors and added errors in result.
+     * @param inputFile file from upload form
+     * @return errors in input file schema
+     */
+    public String getErrors(MultipartFile inputFile) throws JsonProcessingException {
+        //Parameter.Services.ServiceName maps
+        StringBuilder result = new StringBuilder();
+        String inputFileContent = fileService.getFileContent(inputFile);
+        JsonNode inputFileJsonNode = jsonService.getJsonNode(inputFileContent);
+        schemaService.getSchemaErrors(jsonService.getJsonNode(fileService.getFileContent("JsonSchema.json")),inputFileJsonNode).forEach(err -> result.append(err).append("\n"));
+        if(!(result.toString().matches("(?s).*null found, object expected(?s).*") ||
+                result.toString().matches("(?s).*artifacts: is missing but it is required(?s).*"))) {  //check ArtifactObject1-2
+            ArrayList<JsonNode> artifacts = MAPPER.readValue(inputFileContent, Artifacts.class).getArtifacts();
+            for (JsonNode artifact : artifacts) {
+                if (new ArrayList<>(schemaService.getSchemaErrors(schemaService.getJsonSchema(ArtifactObject1.class), artifact))
+                        .get(0).toString()
+                        .equals("$.mvn: is missing but it is required")
+                ) {
+                    schemaService.getSchemaErrors(schemaService.getJsonSchema(ArtifactObject2.class), artifact)
+                            .forEach(err ->
+                                    result.append("$.services.")
+                                            .append(artifacts.indexOf(artifact))
+                                            .append(".")
+                                            .append(err.toString().substring(2))
+                                            .append("\n")
+                            );
+                } else {
+                    schemaService.getSchemaErrors(schemaService.getJsonSchema(ArtifactObject1.class), artifact)
+                            .forEach(err ->
+                                    result.append("$.services.")
+                                            .append(artifacts.indexOf(artifact))
+                                            .append(".")
+                                            .append(err.toString().substring(2))
+                                            .append("\n")
+                            );
+                }
+            }
+        }
+        return result.toString();
     }
-
-
 }
