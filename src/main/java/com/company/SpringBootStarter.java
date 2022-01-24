5c9199735de23f823104e0072e192a83c7364077
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-24 22:45:51 +0300

    refactor

--- src/main/java/com/company/SpringBootStarter.java
+++ src/main/java/com/company/SpringBootStarter.java
@@ -1,11 +1,7 @@
 package com.company;
 
-import com.company.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-
 
 @SpringBootApplication
 public class SpringBootStarter {
