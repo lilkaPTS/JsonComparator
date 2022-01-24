a8175fa1a6b95738f5cf56163e655bd9bcc64b51
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-24 22:44:04 +0300

    creates responseObject for response

--- src/main/java/com/company/controller/JsonRestController.java
+++ src/main/java/com/company/controller/JsonRestController.java
@@ -1,6 +1,7 @@
 package com.company.controller;
 
 import com.company.model.ConfigFile;
+import com.company.model.ResponseObject;
 import com.company.pojo.JsonStructure;
 import com.company.service.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -32,22 +33,28 @@ public class JsonRestController {
     private ComparisonService comparisonService;
 
     @PostMapping("/test")
-    public List<String> testPost(@RequestParam(name = "file1") MultipartFile multipartFile1, @RequestParam(name = "file2") MultipartFile multipartFile2)
+    public ResponseObject testPost(@RequestParam(name = "file1") MultipartFile multipartFile1, @RequestParam(name = "file2") MultipartFile multipartFile2)
             throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        ArrayList<String> result = new ArrayList<>();
-        String errors1 = validationService.getErrors(multipartFile1);
-        String errors2 = validationService.getErrors(multipartFile2);
-        System.out.println(errors1);
-        System.out.println(errors2);
-        if("".equals(errors1) && "".equals(errors2)) {
+        ResponseObject result = new ResponseObject();
+
+        List<String> errors = new ArrayList<>();
+        errors.addAll(validationService.getErrors(multipartFile1));
+        errors.addAll(validationService.getErrors(multipartFile2));
+
+        result.setErrors(errors);
+
+        errors.forEach(System.out::println);
+        System.out.println(errors.size());
+
+        if(errors.isEmpty()) {
             ConfigFile configFile1 = new ConfigFile(mapper.readValue(fileService.getFileContent(multipartFile1), JsonStructure.class));
             ConfigFile configFile2 = new ConfigFile(mapper.readValue(fileService.getFileContent(multipartFile2), JsonStructure.class));
-            result.add(jsonService.getJsonPrettyString(configFile1));
-            result.add(jsonService.getJsonPrettyString(configFile2));
-            System.out.println("Files created successfully");
-            System.out.println(jsonService.getJsonNode(fileService.getFileContent(multipartFile1)).at("/artifacts/0/mvn/0/groupId"));
 
+            result.setConfigFile1(jsonService.getJsonPrettyString(configFile1));
+            result.setConfigFile2(jsonService.getJsonPrettyString(configFile2));
+            result.setMetadata(comparisonService.getInconsistenciesOfMetadata(configFile1, configFile2));
+            System.out.println("всё гуд");
         }
         return result;
     }
