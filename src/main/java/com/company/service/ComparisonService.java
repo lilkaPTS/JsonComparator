e524d6fbf91e370ddda2b04a9217b68bd7ff8326
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-27 20:19:25 +0300

    added function which get simple multipliers

--- src/main/java/com/company/service/ComparisonService.java
+++ src/main/java/com/company/service/ComparisonService.java
@@ -22,6 +22,14 @@ public class ComparisonService {
         return result;
     }
 
+    public List<String> getInconsistenciesOfServices(ConfigFile config1, ConfigFile config2){
+        List<String> result = new ArrayList<>();
+        List<String> additional1 = new ArrayList<>();
+        List<String> additional2 = new ArrayList<>();
+
+        return result;
+    }
+
     public List<String> getInconsistenciesOfOptional(ConfigFile config1, ConfigFile config2) {
         List<String> result = new ArrayList<>();
 
@@ -29,10 +37,50 @@ public class ComparisonService {
     }
 
     public String jsonStringSetter(String key, String value) {
-        return "\"" + key + "\": " + "\"" + value + "\"";
+        return "\"" + key + "\":" + "\"" + value + "\"";
     }
 
     public String jsonStringSetter(String key, int value) {
-        return "\"" + key + "\": " + value;
+        return "\"" + key + "\":" + value;
+    }
+
+    public static List<Integer> getMultipliersV2(int inputNumber) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 2; inputNumber > 1; i++) {
+            if(inputNumber%i==0){
+                inputNumber/=i;
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public static List<Integer> getSimpleNumbers(int limit) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 2; i <= limit; i++) {
+            result.add(i);
+        }
+        for (int i = 2; i*i < limit; i++) {
+            if(result.contains(i))
+                for (int j = i*i; j <= limit; j+=i) {
+                    result.remove(new Integer(j));
+                }
+        }
+        return result;
+    }
+
+    public static List<Integer> getMultipliers(int inputNumber) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> simpleNumbers = getSimpleNumbers(100);
+        for (int simpleNumber : simpleNumbers) {
+            if(inputNumber%simpleNumber==0) {
+                inputNumber /= simpleNumber;
+                result.add(simpleNumber);
+            }
+            if(inputNumber==0) {
+                break;
+            }
+        }
+        return result;
     }
 }
