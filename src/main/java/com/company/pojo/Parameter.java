823b20c832d6459e315df7c6d8f10e9f4e80b80c
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-02-14 19:25:40 +0300

    fixed json structure

--- src/main/java/com/company/pojo/Parameter.java
+++ src/main/java/com/company/pojo/Parameter.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Parameter {
-    @JsonProperty(value = "common")
-    private Common common;
+    @JsonProperty(value = "common", required = true)
+    private Map<String, String> common;
     @JsonProperty(value = "services")
-    private Services services;
+    private Map<String, Map<String,String>> services;
 }
