2aebb973961342126a18c086d54b5880f9fc18e6
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-02-20 14:50:00 +0300

    added toJsonView function which can transform ugly json object to beautiful

--- src/main/java/com/company/model/ResponseView.java
+++ src/main/java/com/company/model/ResponseView.java
@@ -15,26 +15,51 @@ public class ResponseView {
     private List<String> configFile1 = new ArrayList<>();
     private List<String> configFile2 = new ArrayList<>();
 
+    public ResponseView toJsonView() {
+        toJsonViewHelper(configFile1);
+        toJsonViewHelper(configFile2);
+        return this;
+    }
+
+    public List<String> toJsonViewHelper(List<String> list) {
+        StringBuilder spaceCounter = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).matches(".*}.*") || list.get(i).matches(".*].*")) {
+                spaceCounter.delete(0,3);
+            }
+            list.set(i, spaceCounter+list.get(i));
+            if(list.get(i).matches(".*\\{.*") || list.get(i).matches(".*\\[.*")) {
+                spaceCounter.append("   ");
+            }
+        }
+        return list;
+    }
+
     public void add(String str) {
         this.add1(str);
         this.add2(str);
     }
+
     public void add1(String str) {
         configFile1.add(str);
     }
+
     public void add2(String str) {
         configFile2.add(str);
     }
+
     public void add1(String[] strings) {
         for (String s: strings) {
             add1(s);
         }
     }
+
     public void add2(String[] strings) {
         for (String s: strings) {
             add2(s);
         }
     }
+
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
