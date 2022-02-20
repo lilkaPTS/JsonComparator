653389816d039146fae0a6ebba1e118967a68f29
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-02-20 15:28:52 +0300

    routing renamed

--- src/main/java/com/company/controller/JsonRestController.java
+++ src/main/java/com/company/controller/JsonRestController.java
@@ -2,7 +2,6 @@ package com.company.controller;
 
 import com.company.model.ConfigFile;
 import com.company.model.ResponseObject;
-import com.company.model.ResponseView;
 import com.company.pojo.JsonStructure;
 import com.company.service.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,17 +20,13 @@ import java.util.*;
 public class JsonRestController {
 
     @Autowired
-    private SchemaService schemaService;
-    @Autowired
     private FileService fileService;
-    @Autowired
-    private JsonService jsonService;
     @Autowired
     private ValidationService validationService;
     @Autowired
     private ComparisonService comparisonService;
 
-    @PostMapping("/test")
+    @PostMapping("/validating")
     public ResponseObject testPost(@RequestParam(name = "file1") MultipartFile multipartFile1, @RequestParam(name = "file2") MultipartFile multipartFile2)
             throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -40,12 +35,10 @@ public class JsonRestController {
         errors.addAll(validationService.getErrors(multipartFile1));
         errors.addAll(validationService.getErrors(multipartFile2));
         result.setErrors(errors);
-        errors.forEach(System.out::println);
         if(errors.isEmpty()) {
             ConfigFile configFile1 = new ConfigFile(mapper.readValue(fileService.getFileContent(multipartFile1), JsonStructure.class));
             ConfigFile configFile2 = new ConfigFile(mapper.readValue(fileService.getFileContent(multipartFile2), JsonStructure.class));
-           // System.out.println(jsonService.getJsonPrettyString(configFile1).split("\"artifacts\" : \\[ \\{")[0].split("\n")[0].replace("\r", " ") + "213");
-            result.setResponseView(comparisonService.execute(configFile1, configFile2));
+            result.setResponseView(comparisonService.execute(configFile1, configFile2).toJsonView());
         }
         return result;
     }
