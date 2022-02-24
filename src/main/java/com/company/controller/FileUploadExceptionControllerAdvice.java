15a1bf69180fd2cb5317bbaf4a3d013a21dec925
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-02-25 02:25:41 +0300

    MaxUploadSizeExceededException handled

--- src/main/java/com/company/controller/FileUploadExceptionControllerAdvice.java
+++ src/main/java/com/company/controller/FileUploadExceptionControllerAdvice.java
@@ -1,18 +1,24 @@
 package com.company.controller;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.company.model.ResponseObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
 
-@ControllerAdvice
+
+@RestControllerAdvice
 public class FileUploadExceptionControllerAdvice {
 
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleMaxSizeException(MaxUploadSizeExceededException exc, HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("$.Error size, file too large. Uploaded file must be less than 10 kb");
-        return "responsePage";
+    @ExceptionHandler(value = {MaxUploadSizeExceededException.class})
+    public ResponseEntity<ResponseObject> handleMaxSizeException() {
+        ResponseObject result = new ResponseObject();
+        result.setErrors(Collections.singletonList("$.Error size, file too large. Uploaded file must be less than 10 mb"));
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        return new ResponseEntity<>(result, responseHeaders, HttpStatus.OK);
     }
 }
